51b8eabb1dd93f70786d55fd3d04e8e2cc46ca70
ryanslam <Ryanshiau1@gmail.com>
2024-03-04 16:50:04 -0800

    imu_test: add PID and PWM computation
    This commit adds the computation for PID and PWM. PID is implemented using the PID library. Using the output,
    we compute the PWM for the dc motors based on angular error of our seasaw.

--- imu_test/imu_test.ino
+++ imu_test/imu_test.ino
@@ -5,8 +5,14 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+
+double input_angle, angle_output;
+double set_angle = 0;
+double kp = 1.0, ki = 1.0, kd = 1.0;
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+PID seasaw_pid(&input_angle, &angle_output, &set_angle, kp, ki, kd, DIRECT);
 
 void setup(void) 
 {
@@ -24,10 +30,23 @@ void setup(void)
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
+
+  // Initialize PID
+  pid.SetMode(AUTOMATIC);
+  pid.SetSampleTime(10); 
   
   delay(1000);
     
   bno.setExtCrystalUse(true);
+}
+
+float set_point_position(float max_dist, float set_point_dist1, float set_point_dist2){
+
+}
+// Only used if we want to manually create the PID controller without the PID library.
+float calculate_pulley_pwm(float angle, float desired_angle){
+  float err = abs(desired_angle - angle);
+  return err;
 }
 
 void loop(void) 
@@ -37,8 +56,13 @@ void loop(void)
 
   bno.getEvent(&event);
 
-  angle = event.orientation.y;
+  input_angle = event.orientation.y;            // From -30 degrees to 30 degrees.
+  set_angle = 20                                // Desired angle.
+  
+  // Compute the PID based on angle error.
+  seasaw_pid.Compute();
 
+  // Set the direction of the motor using the input pins.
   if(angle > 0){
     digitalWrite(IN_ONE, HIGH);
     digitalWrite(IN_TWO, LOW);
@@ -51,6 +75,14 @@ void loop(void)
     digitalWrite(IN_ONE, LOW);
     digitalWrite(IN_TWO, LOW);
   }
+
+  int motorSpeed = map(output, -90, 90, 0, 255);
+  motorSpeed = constrain(motorSpeed, 0, 255);
+
+  // Apply PWM signal to control motor speed
+  analogWrite(motorPin, motorSpeed);  
+
+  // Map PID output to pwm.
   
   delay(100);
 }
