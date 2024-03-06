c0f593e02a2e3a1324ee15562ca0b644a931c93a
ryanslam <Ryanshiau1@gmail.com>
2024-03-05 18:20:52 -0800

    Seasaw_Position_Control: Fix bugs and add set point functionality
    This commit adds set point functionality using a ultrasonic sensor to measure the distance of our
    setpoint object. Additionally this commit resolves bugs related to previously incorrect initialization of
    the PID library. The name of the file containing the controller has also been changed to better represent the project.

--- Seasaw_Position_Control/Seasaw_Position_Control.ino
+++ Seasaw_Position_Control/Seasaw_Position_Control.ino
@@ -5,6 +5,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <PID_v1.h>
 
 double input_angle, angle_output;
 double set_angle = 0;
@@ -14,35 +15,45 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 PID seasaw_pid(&input_angle, &angle_output, &set_angle, kp, ki, kd, DIRECT);
 
-void setup(void) 
-{
+// Ultrasonic sensor pins.
+const int set_trig_pin = 9;
+const int set_echoPin = 10;
+
+// Motor driver pins.
+const int motor_pin = 11;
+
+float set_duration, set_position;
+
+void setup() {
   Serial.begin(9600);
 
-  // Set motor pins for direction
+  Serial.print("starting");
+  // Set motor pins for direction.
   pinMode(IN_ONE, OUTPUT);
   pinMode(IN_TWO, OUTPUT);
+
+  // Pins for set position sensor.
+  pinMode(set_trig_pin, OUTPUT);
+  pinMode(set_echoPin, INPUT);
+
   Serial.println("Orientation Sensor Test"); Serial.println("");
   
   /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
+  if(!bno.begin()){
+  /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
 
   // Initialize PID
-  pid.SetMode(AUTOMATIC);
-  pid.SetSampleTime(10); 
+  seasaw_pid.SetMode(AUTOMATIC);
+  seasaw_pid.SetSampleTime(10); 
   
   delay(1000);
     
   bno.setExtCrystalUse(true);
 }
 
-float set_point_position(float max_dist, float set_point_dist1, float set_point_dist2){
-
-}
 // Only used if we want to manually create the PID controller without the PID library.
 float calculate_pulley_pwm(float angle, float desired_angle){
   float err = abs(desired_angle - angle);
@@ -57,17 +68,29 @@ void loop(void)
   bno.getEvent(&event);
 
   input_angle = event.orientation.y;            // From -30 degrees to 30 degrees.
-  set_angle = 20                                // Desired angle.
+  set_angle = 20;                               // Desired angle.
+
+  // Retrieve distance of set point.
+  digitalWrite(set_trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(set_trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(set_trig_pin, LOW);
+
+  set_duration = pulseIn(set_echoPin, HIGH);
+  set_position = (set_duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(set_position);
   
   // Compute the PID based on angle error.
   seasaw_pid.Compute();
 
   // Set the direction of the motor using the input pins.
-  if(angle > 0){
+  if(input_angle > 0){
     digitalWrite(IN_ONE, HIGH);
     digitalWrite(IN_TWO, LOW);
   }
-  else if(angle < 0){
+  else if(input_angle < 0){
     digitalWrite(IN_ONE, LOW);
     digitalWrite(IN_TWO, HIGH);
   }
@@ -76,13 +99,12 @@ void loop(void)
     digitalWrite(IN_TWO, LOW);
   }
 
-  int motorSpeed = map(output, -90, 90, 0, 255);
-  motorSpeed = constrain(motorSpeed, 0, 255);
+  // Generate PWM for DC Motors (Based on angle).
+  int motor_speed = map(angle_output, -30, 30, 0, 255);
+  motor_speed = constrain(motor_speed, 0, 255);
 
   // Apply PWM signal to control motor speed
-  analogWrite(motorPin, motorSpeed);  
-
-  // Map PID output to pwm.
+  analogWrite(motor_pin, motor_speed);
   
   delay(100);
 }
