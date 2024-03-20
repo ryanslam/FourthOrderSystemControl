91cb7b1e0784c51db87fb009335a782c94755a81
ryanslam <Ryanshiau1@gmail.com>
2024-03-19 22:11:52 -0700

    Seesaw_Position_Control: Add fully functional PID control
    This commit adds the fully functioning software to control our mass controlled seesaw system.

--- Seasaw_Position_Control/Seasaw_Position_Control.ino
+++ Seasaw_Position_Control/Seasaw_Position_Control.ino
@@ -1,111 +1,118 @@
-#define IN_ONE 13
-#define IN_TWO 12
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <PID_v1.h>
 
-double input_angle, angle_output;
-double set_angle = 0;
-double kp = 1, ki = 0.0, kd = 5;
-float integral = 0;
-float prev_error = 0;
-  
+// Output PINS
+const int potPin = A0;            // Analog pin for potentiometer
+
+// PIN A
+const int motorEnablePinA = 10;     // PWM pin to control motor speed (ENA on L298N)
+// PIN B
+const int motorEnablePinB = 11;     // PWM pin to control motor speed (ENA on L298N)
+const int motorDirectionPin1 = 13;  // Input 1 (IN1 on L298N)
+const int motorDirectionPin2 = 12;  // Input 2 (IN2 on L298N) 
+
+// IMU Global Variables
+double currentAgngle = 0.0;
+double maxAngle = 13.0;
+double minAngle = -13.0;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-// PID seasaw_pid(&input_angle, &angle_output, &set_angle, kp, ki, kd, DIRECT);
+// Control Variables
+double error = 0.0;
+double errorCalc = 0.0;
+double kp = 0.01;
+double ki = 0.0;
+double kd = 50.0;
+float integral = 0;
+float prev_error = 0;
 
-// // Ultrasonic sensor pins.  
-
-// Motor driver pins.
-const int motor_pin = 11;
-
-float set_duration, set_position;
-
+// Motor Global Variables
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(2000000);
 
-  // Set motor pins for direction.
-  pinMode(IN_ONE, OUTPUT);
-  pinMode(IN_TWO, OUTPUT);
+  // Motors
+  pinMode(potPin, INPUT);
+  pinMode(motorEnablePinA, OUTPUT);
+  pinMode(motorEnablePinB, OUTPUT);
+  pinMode(motorDirectionPin1, OUTPUT);
+  pinMode(motorDirectionPin2, OUTPUT);
 
-  // // Pins for set position sensor.
-  // pinMode(set_trig_pin, OUTPUT);
-  // pinMode(set_echoPin, INPUT);
-
+  // IMU Sensors
   Serial.println("Orientation Sensor Test"); Serial.println("");
-  
   /* Initialise the sensor */
   if(!bno.begin()){
   /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-
-  // // Initialize PID
-  // seasaw_pid.SetMode(AUTOMATIC);
-  // seasaw_pid.SetSampleTime(10); 
-  
   delay(1000);
-    
   bno.setExtCrystalUse(true);
+
 }
 
-// Only used if we want to manually create the PID controller without the PID library.
-float calculate_pulley_pwm(float angle, float desired_angle){
-  float err = abs(desired_angle - angle);
-  return err;
-}
-
-void loop(void) 
-{
+void loop() {
   /* Get a new sensor event */ 
   sensors_event_t event; 
-
   bno.getEvent(&event);
+  currentAgngle = event.orientation.y;   // From -20 degrees to 20 degrees.
 
-  input_angle = event.orientation.y;            // From -30 degrees to 30 degrees.
-  set_angle = 20;                               // Desired angle.
-  
-  float error = abs(set_angle - input_angle);
+  // User Input Potentiometer
+  int rawAngleRead = analogRead(potPin);
+  int desiredAngle = map(rawAngleRead,0,1023,-20,20);
+
+  // Control Loop
+  errorCalc =  desiredAngle - currentAgngle;
+  error = abs(errorCalc);
   integral = integral + (error*100);
   float derivative = (error-prev_error)/100;
   float output = (kp*error)+(ki*integral)+(kd*derivative);
-  prev_error = error;
-  // Serial.print("Angle Error: ");
-  // Serial.println(abs(set_angle-input_angle));
-  // Serial.print("Current Angle: ");
-  // Serial.println(input_angle);
-  // Compute the PID based on angle error.
-  // seasaw_pid.Compute();
-
-  // Set the direction of the motor using the input pins.
-  if(set_angle-input_angle > 1){
-    digitalWrite(IN_ONE, HIGH);
-    digitalWrite(IN_TWO, LOW);
-  }
-  else if(set_angle-input_angle < -1){
-    digitalWrite(IN_ONE, LOW);
-    digitalWrite(IN_TWO, HIGH);
-  }
-  else{
-    digitalWrite(IN_ONE, LOW);
-    digitalWrite(IN_TWO, LOW);
-  }
-
-  // // Generate PWM for DC Motors (Based on angle).
-  Serial.print("Current Angle: ");
-  Serial.println(input_angle);
-  Serial.print("error: ");
-  Serial.println(error);
-  int motor_speed = map(output, 0, 180, 80, 100);
-  motor_speed = constrain(motor_speed, 80, 100);
-  Serial.print("Motor Speed: ");
-  Serial.println(motor_speed);
-  // Apply PWM signal to control motor speed
-  analogWrite(motor_pin, motor_speed);
   
-  delay(100);
+  prev_error = error;
+  
+  // Motor Speed Mapping
+  int motorSpeed = map(abs(output), 0, 10, 120, 255);
+
+  if ( (errorCalc < -1)  ) {
+     // Controlling the direction of the mototor
+    analogWrite(motorEnablePinA, motorSpeed);
+    analogWrite(motorEnablePinB, 50);
+    digitalWrite(motorDirectionPin1, LOW);
+    digitalWrite(motorDirectionPin2, HIGH);
+    Serial.println("First loop");
+    
+  } else if ( (errorCalc > 1) ){
+    // Controlling the direction of the mototor
+    analogWrite(motorEnablePinA, 50);
+    analogWrite(motorEnablePinB, motorSpeed);
+    digitalWrite(motorDirectionPin1, HIGH);
+    digitalWrite(motorDirectionPin2, LOW);
+    Serial.println("second loop");
+  } else {
+    analogWrite(motorEnablePinA, 0);
+    analogWrite(motorEnablePinB, 0);
+    digitalWrite(motorDirectionPin1, LOW);
+    digitalWrite(motorDirectionPin2, LOW);
+  }
+
+  // Printing on Serial Monitor 
+  Serial.print("Current Angle: ");
+  Serial.print(currentAgngle);
+  
+  Serial.print("| Desired Angle: ");
+  Serial.print(desiredAngle);
+
+  Serial.print("| Error: ");
+  Serial.print(errorCalc);
+
+  Serial.print("| Motore Speed: ");
+  Serial.print(motorSpeed);
+
+  Serial.print("| output: ");
+  Serial.println(output);
+
+  
+
 }
